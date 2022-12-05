0020ee17ea17fba2e70afb42dbe23e1d2a272b89
dahalh <dahalh@outlook.com>
2022-12-05 21:30:58 +1100

    User model created

---
+++ models/User.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    min: 2,
+    max: 50,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    min: 2,
+    max: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 5,
+  },
+  picture: {
+    type: String,
+    default: "",
+  },
+  friends: {
+    type: Array,
+    default: [],
+  },
+  location: String,
+  occupation: String,
+  viewedProfile: Number,
+  impressions: Number,
+});
